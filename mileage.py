def51fe8cddf049499a2cf1df9bbcca2e91528af
craighillelson <craighillelson@gmail.com>
2019-12-20 08:50:38 -0500

    removed unnecessary backslash

--- mileage.py
+++ mileage.py
@@ -11,7 +11,7 @@ NOW = datetime.datetime.now()
 YEAR = NOW.year
 
 # define variables
-MILEAGE_TO_DATE = 'How many miles have you run this year? ' \
+MILEAGE_TO_DATE = 'How many miles have you run this year? '\
                   '(please enter an integer) '
 MILEAGE_GOAL = 'What is your goal for the year? '
 NOT_AN_INTEGER = 'That won\'t work. Please enter an integer. '
@@ -45,11 +45,11 @@ DIFF_MILEAGE = GOAL - MILES
 if DIFF_MILEAGE > 0:
     MILES_DAY = float(DIFF_MILEAGE) / float(DELTA.days)
     GOAL = int(GOAL)
-    print(f'With {DAYS_LEFT} days left in the year, you\'ll need to' \
-          f'average {"{0:.2}".format(MILES_DAY)} miles per day to hit ' \
+    print(f'With {DAYS_LEFT} days left in the year, you\'ll need to'
+          f'average {"{0:.2}".format(MILES_DAY)} miles per day to hit '
           f'your {GOAL} mile goal.')
 else:
-    print(f'Congratulations! You hit your mileage goal with {DAYS_LEFT} days ' \
+    print(f'Congratulations! You hit your mileage goal with {DAYS_LEFT} days '
           'left in the year.')
 
 print(RTN())
