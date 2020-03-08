256cdbd154ec6122441a35f5d0faf7e33ad223a7
craighillelson <craighillelson@gmail.com>
2020-03-08 10:44:26 -0400

    update mileage.py
    employed a function to make code easier to read

--- mileage.py
+++ mileage.py
@@ -6,6 +6,19 @@ from datetime import date
 
 RTN = lambda: '\n'
 
+def prompt_user(a, b):
+    """
+    get user input, return an error if what is entered is not an integer or
+    float
+    """
+    while a is None:
+        try:
+            a = float(input(b))
+        except ValueError:
+            print(NOT_AN_INTEGER)
+
+    return a
+
 # figure out what year it is
 NOW = datetime.datetime.now()
 YEAR = NOW.year
@@ -14,24 +27,15 @@ YEAR = NOW.year
 MILEAGE_TO_DATE = 'How many miles have you run this year? '\
                   '(please enter an integer) '
 MILEAGE_GOAL = 'What is your goal for the year? '
-NOT_AN_INTEGER = 'That won\'t work. Please enter an integer. '
+NOT_AN_INTEGER = 'That won\'t work. Please enter an integer or a float. '
 MILES = None
 GOAL = None
 
 print(RTN())
 
 # prompt user for mileage to date and mileage goal for the year
-while MILES is None:
-    try:
-        MILES = float(input(MILEAGE_TO_DATE))
-    except ValueError:
-        print(NOT_AN_INTEGER)
-
-while GOAL is None:
-    try:
-        GOAL = float(input(MILEAGE_GOAL))
-    except ValueError:
-        print(NOT_AN_INTEGER)
+MILES = prompt_user(MILES, MILEAGE_TO_DATE)
+GOAL = prompt_user(GOAL, MILEAGE_GOAL)
 
 # calculate how many days are left in the year
 TODAY = date.today()
@@ -44,8 +48,8 @@ DAYS_LEFT = DELTA.days
 DIFF_MILEAGE = GOAL - MILES
 if DIFF_MILEAGE > 0:
     MILES_DAY = float(DIFF_MILEAGE) / float(DELTA.days)
-    GOAL = int(GOAL)
-    print(f'With {DAYS_LEFT} days left in the year, you\'ll need to'
+    GOAL = float(GOAL)
+    print(f'With {DAYS_LEFT} days left in the year, you\'ll need to '
           f'average {"{0:.2}".format(MILES_DAY)} miles per day to hit '
           f'your {GOAL} mile goal.')
 else:
